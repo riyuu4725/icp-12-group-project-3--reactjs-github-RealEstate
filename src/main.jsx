2ced47b214b46cd0fb458bc2162a4b5df9381ef9
AshishSMathpati <ashishsmathpati@gmail.com>
2026-01-06 11:31:29 +0530

    home and about page tested successfully

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,19 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import {BrowserRouter, Routes , Route } from 'react-router'
+import About from './view/About/About'
+import Home from './view/Home/Home'
 
 
 createRoot(document.getElementById('root')).render(
-    
+<>
+
+<BrowserRouter>
+<Routes>
+    <Route  path='/' element={<Home/>} />
+    <Route  path='/about' element={<About/>} />
+</Routes>
+</BrowserRouter>
+
+</>
 )
